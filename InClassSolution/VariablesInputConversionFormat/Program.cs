64c99e6bbc7d2de916c5c3b73dc42b77d96244fd
Michael Brickey <John.M.Brickey-1@ou.edu>
2021-02-02 18:32:36 -0600

    did a whole bunch of goodness right here

--- InClassSolution/VariablesInputConversionFormat/Program.cs
+++ InClassSolution/VariablesInputConversionFormat/Program.cs
@@ -6,7 +6,24 @@ namespace VariablesInputConversionFormat
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string firstName, lastName;
+            double gpa;
+
+            Console.WriteLine("Please enter your first name >>");
+            firstName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your last name >>");
+            lastName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your gpa >>");
+            string gpaAsString = Console.ReadLine();
+            gpa = Convert.ToDouble(gpaAsString);
+
+            gpa = gpa + .5; //we bumping everybody by 0.5 for giggles
+
+            string output = "Hello" + firstName + " " + lastName + "! " + " we boosted your gpa to " + gpa.ToString("N3");
+
+            Console.WriteLine(output); 
         }
     }
 }
