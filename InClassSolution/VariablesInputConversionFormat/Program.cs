6800a64a287581ebfe95062f972352e45965470a
Michael Brickey <John.M.Brickey-1@ou.edu>
2021-02-02 18:18:25 -0600

    Created a new solution for variables inouts conversion

---
+++ InClassSolution/VariablesInputConversionFormat/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VariablesInputConversionFormat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
